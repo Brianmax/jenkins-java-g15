152d38f4672e548cf2a498a62a512027ddccd67e
george <george.maxi@ucsp.edu.pe>
2026-05-13 22:18:23 -0500

    fix error

--- src/main/java/com/example/todos/service/TodoService.java
+++ src/main/java/com/example/todos/service/TodoService.java
@@ -54,6 +54,7 @@ public class TodoService {
         Todo saved = todoRepository.save(todo);
         log.info("operation=todo.created todoId={}", saved.getId());
         System.out.println("jafkldjlkfjlkdslj");
+
         return toResponse(saved);
     }
 
